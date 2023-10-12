1434e64b9621f7f352671ed44b5a21e8b1bb5f76
leeHyeonJun1109 <axtivi1109@gmail.com>
2023-10-12 11:04:43 +0900

    Feat: "타워 Target 발사까지"

---
+++ Project_A/Assets/Scripts/TowerSystem/Projectile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float moveSpeed;
+    public float damagedAmount;
+    private bool hasDamaged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * moveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" && !hasDamaged)
+        {
+            hasDamaged = true;
+        }
+
+        Destroy(gameObject);
+    }
+
+}
